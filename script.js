75ce70c00bbe2a69ec961a5b088018ff1fd1e8b3
FattyRatty <60388760+emilasbringer@users.noreply.github.com>
2021-10-21 13:17:49 +0200

    boom

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let number = 7;
-let guess = prompt('Kan du gissa på det hemliga numret?');
+// let guess = prompt('Kan du gissa på det hemliga numret?');
+let run = true;
 
+const button = document.querySelector("#buttonElement");
+const input = document.querySelector("#inputElement");
+const list = document.querySelector("#listElement");
+
+button.addEventlistener('click', (e) => {
+    const inputValue = inputElement.value;
+});
+
+while (run) {
 if (number == guess) {
-    alert('Otroligt, du gissade rätt!');
-} else {
-    // Du gissade fel
+    
+    run = false;
+} 
+else if (number > guess) {
+    alert('För litet!');
+}
+else {
+    alert('För högt!');
+}
 }
